8629d54b5f3740b2ec67d856a72183b60ff95b1d
seonghyeoncho <chosh040@naver.com>
2021-07-31 17:52:35 +0900

    add video

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -19,8 +19,7 @@ let config = {
   },
   media: {
     video: {
-      width: { min: 320, max: 640 },
-      height: { min: 240, max: 480 },
+      height: { max: 240 },
       frameRate: { min: 8, max: 30 },
       maxBandwidth: 500,
       codec: 'H264'
@@ -33,16 +32,14 @@ const videoAttrs = {
   className: "remote-video",
   autoPlay : true,
   muted : true,
-  style :"z-index:1; background: rgba(0, 0, 0, 0.5);"
+  style :"background: rgba(0, 0, 0, 0.5); border-radius: 10px; margin-right: 15px;"
 };
 
+
 export default function Video(props) {
-  console.log(props);
   const room = props.id;
   const [videoState, setVideoState] = useState(false);
   const [audioState, setAudioState] = useState(false);
-
-  const otherVideos = document.getElementById('otherVideos');
  
   const listener = {
     onConnect(chid) {
@@ -64,32 +61,32 @@ export default function Video(props) {
     },
     onStat(result) {},
     onRoomEvent(result){
-        switch (result.event) {
-          case 'join':
-            if (!remonRoom[result.channel.id]) {
-              remonRoom[result.channel.id] = true;
+      switch (result.event) {
+        case 'join':
+          if (!remonRoom[result.channel.id]) {
+            remonRoom[result.channel.id] = true;
 
-              let newVideo = document.createElement('video');
+            let newVideo = document.createElement('video');
 
-              videoAttrs.id = result.channel.id.replace(":","-");
+            videoAttrs.id = result.channel.id.replace(":","-");
 
-              Object.keys(videoAttrs).forEach(key => newVideo.setAttribute(key, videoAttrs[key]));
+            Object.keys(videoAttrs).forEach(key => newVideo.setAttribute(key, videoAttrs[key]));
 
-              config.view.remote = `#${newVideo.id}`;
-              newVideo.remon = new Remon({ config });
+            config.view.remote = `#${newVideo.id}`;
+            newVideo.remon = new Remon({ config });
 
-              otherVideos.appendChild(newVideo);
-              newVideo.remon.joinCast(newVideo.id.replace("-",":"));
-            }
-            break;
-          case 'leave':
-            if(remonRoom[result.channel.id] && result.channel.id !== remon.getChannelId()){
-              let video = document.getElementById(result.channel.id.replace(":","-"));
-              otherVideos.removeChild(video);
-              delete remonRoom[result.channel.id]
-            }
-            break;
+            document.getElementById('otherVideos').appendChild(newVideo);
+            newVideo.remon.joinCast(newVideo.id.replace("-",":"));
           }
+          break;
+        case 'leave':
+          if(remonRoom[result.channel.id] && result.channel.id !== remon.getChannelId()){
+            let video = document.getElementById(result.channel.id.replace(":","-"));
+            document.getElementById('otherVideos').removeChild(video);
+            delete remonRoom[result.channel.id]
+          }
+          break;
+        }
       console.log(`EVENT FIRED: onRoomEvent channel Id : ${remon.getChannelId()}`)
       console.log(`EVENT FIRED: onRoomEvent: ${JSON.stringify(result)}`)
     }
@@ -104,7 +101,7 @@ export default function Video(props) {
           let video = document.getElementById(id.replace(":","-"));
 
           if ( video && video.remon ) {
-            otherVideos.removeChild(video);
+            document.getElementById('otherVideos').removeChild(video);
           }
         }
         delete remonRoom[id];
@@ -127,12 +124,15 @@ export default function Video(props) {
           Object.keys(videoAttrs).forEach(key => newVideo.setAttribute(key, videoAttrs[key]))
           config.view.remote = `#${newVideo.id}`
           newVideo.remon = new Remon({ config })
-          otherVideos.appendChild(newVideo);
+          document.getElementById('otherVideos').appendChild(newVideo);
 
           await newVideo.remon.joinCast(newVideo.id.replace("-",":"));
         }
       })
     }
+  }
+  const onClick = () => {
+    start();
   }
   
   const hadleMuteMyVideo = () => {
@@ -143,25 +143,31 @@ export default function Video(props) {
     remon.muteLocalAudio(!audioState);
     setAudioState(!audioState);
   }
-
   useEffect(() => {
-    if(!isConnected) start();
+    start();
   }, []);
 
   return (
-    <div>
-      <div>
-        <div style={{ display: 'flex' }}>
-          <div className="row" id="otherVideos"/>
-          <video
-            id="myVideo"
-            className="remote-video" 
-            autoPlay
-            muted
-            style={{backgroundColor: 'black'}}
-          />
-          <button onClick={hadleMuteMyVideo}>{ !videoState ? '화면 끄기' : '화면 켜기'}</button>
-          <button onClick={handleMuteMyAudio}>{ !audioState ? '음소거' : '음소거 해제'}</button>
+     <div style={{height: '100%', padding: '20px'}}>
+      <div style={{height: '100%', display: 'flex', flexWrap: 'wrap', alignContent: 'space-between'}}>
+        <div style={{ width: '100%'}}>
+          <div style={{ display: 'flex', justifyContent: 'center', width: '100%'}}>
+            <video
+              id="myVideo"
+              className="remote-video" 
+              autoPlay
+              muted
+              style={{backgroundColor: 'black', borderRadius: '10px'}}
+            />
+            <div className="row" id="otherVideos" style={{display: 'flex'}} video={{ padding: '100px'}}/>
+          </div>
+        </div> 
+        <div style={{ width: '100%'}}>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <button onClick={hadleMuteMyVideo} style={{marginRight: '10px'}}>{ !videoState ? '화면 끄기' : '화면 켜기'}</button>
+            <button onClick={handleMuteMyAudio} style={{marginRight: '10px'}}>{ !audioState ? '음소거' : '음소거 해제'}</button>
+            <button onClick={onClick}>종료</button>
+          </div>
         </div>
       </div>
     </div>
