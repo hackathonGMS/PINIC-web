1b9389ef492e667d3e321d9b77f53ea339396e30
Margarets00 <minj01134@gmail.com>
2021-07-31 23:47:25 +0900

    dd

--- src/components/Chatting/TodoBlock/index.js
+++ src/components/Chatting/TodoBlock/index.js
@@ -1,28 +1,28 @@
 import { Box, Button, Center, CloseButton, HStack, Input, Spacer, Tag, TagCloseButton, Text, Wrap } from "@chakra-ui/react";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import db from "../firbase";
-import './index.css';
+import "./index.css";
 
-export const Todo = ({roomId}) => {
+export const Todo = ({ roomId }) => {
   const [todoList, setTodoList] = useState([]);
-  const [ todo, setTodo ] = useState({
-    value: '',
+  const [todo, setTodo] = useState({
+    value: "",
     done: false,
   });
   const notConfirmList = () => {
     return todoList.filter((todo) => todo.done !== false);
-  }
+  };
   const confirmedList = () => {
     return todoList.filter((todo) => todo.done !== true);
-  }
+  };
   const onC = () => {
-    if (todo.value !== '') {
+    if (todo.value !== "") {
       db.collection("Chatting")
-      .doc(roomId)
-      .update({
-          todo: [...todoList, todo]
-        })
-      setTodo({ value: ''});
+        .doc(roomId)
+        .update({
+          todo: [...todoList, todo],
+        });
+      setTodo({ value: "" });
     }
   };
   const onChange = (e) => {
@@ -35,8 +35,8 @@ export const Todo = ({roomId}) => {
       if (todoList[i].value === t) {
         const data = {
           done: state,
-          value: todoList[i].value
-        }
+          value: todoList[i].value,
+        };
         newTodoList.push(data);
       } else newTodoList.push(todoList[i]);
     }
@@ -44,55 +44,54 @@ export const Todo = ({roomId}) => {
     db.collection("Chatting")
       .doc(roomId)
       .update({
-        todo: [...newTodoList]
-      })
-  }
+        todo: [...newTodoList],
+      });
+  };
 
   useEffect(() => {
-    db.collection('Chatting')
+    db.collection("Chatting")
       .doc(roomId)
       .onSnapshot((d) => {
         if (d.data().todo !== undefined) {
           setTodoList(d.data().todo);
         }
       });
-    
   }, []);
   console.log(todoList);
   return (
     <Box w="full" bg="black" borderRadius="10px" p="10px">
       <Box w="full" h="1px" bg="black" mt="3px" mb="10px"></Box>
-      <div className='scroll' style={{ display: 'flex', flexDirection: 'column', minHeight: '60px', maxHeight: '160px', overflowY: 'auto', paddingRight: '5px'}}>
-        {
-          notConfirmList().map((todo, index) => (
-            <div key={index} style={{ display: 'flex', justifyContent: 'space-between'}}>
-              {todo.value}
-              <div style={{display: 'flex', alignSelf: 'center'}}>
-                <input type="checkbox" checked={todo.done} onChange={() => changeTodoState(todo.value, false)}/>
-              </div>
+      <div
+        className="scroll"
+        style={{ display: "flex", flexDirection: "column", minHeight: "60px", maxHeight: "160px", overflowY: "auto", paddingRight: "5px" }}>
+        {notConfirmList().map((todo, index) => (
+          <div key={index} style={{ display: "flex", justifyContent: "space-between" }}>
+            {todo.value}
+            <div style={{ display: "flex", alignSelf: "center" }}>
+              <input type="checkbox" checked={todo.done} onChange={() => changeTodoState(todo.value, false)} />
             </div>
-          ))
-        }
-        {
-          confirmedList().map((todo, index) => (
-            <div key={index} style={{ display: 'flex', justifyContent: 'space-between'}}>
-              {todo.value}
-              <div style={{display: 'flex', alignSelf: 'center'}}>
-                <input type="checkbox" checked={todo.done} onChange={() => changeTodoState(todo.value, true)}/>
-              </div>
+          </div>
+        ))}
+        {confirmedList().map((todo, index) => (
+          <div key={index} style={{ display: "flex", justifyContent: "space-between" }}>
+            {todo.value}
+            <div style={{ display: "flex", alignSelf: "center" }}>
+              <input type="checkbox" checked={todo.done} onChange={() => changeTodoState(todo.value, true)} />
             </div>
-          ))
-        }
+          </div>
+        ))}
       </div>
       <Box w="full" h="1px" bg="black" mt="38px" mb="9px"></Box>
       <Center>
         <div>
-          <input style={{ color: 'black', width: '100%', marginBottom: '5px' }} value={todo.value} onChange={onChange}/>
-          <div style={{display: 'flex', justifyContent: 'center'}}>
-            <button onClick={onC} style={{}}>add todo</button>
+          <input style={{ color: "black", width: "100%", marginBottom: "5px" }} value={todo.value} onChange={onChange} />
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <button onClick={onC} style={{}}>
+              add todo
+            </button>
           </div>
         </div>
       </Center>
     </Box>
   );
-}
+};
